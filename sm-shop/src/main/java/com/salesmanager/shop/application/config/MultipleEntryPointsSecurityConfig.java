db04f67860a7b8ee97a7f9d852a83b0539c45b8c
adriano.santos.bm <adriano.santos.bm@gmail.com>
2021-04-28 17:13:56 -0300

    local

--- sm-shop/src/main/java/com/salesmanager/shop/application/config/MultipleEntryPointsSecurityConfig.java
+++ sm-shop/src/main/java/com/salesmanager/shop/application/config/MultipleEntryPointsSecurityConfig.java
@@ -1,7 +1,13 @@
 package com.salesmanager.shop.application.config;
 
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
@@ -12,10 +18,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -38,6 +48,7 @@ import com.salesmanager.shop.store.security.services.CredentialsServiceImpl;
  */
 @Configuration
 @EnableWebSecurity
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
 public class MultipleEntryPointsSecurityConfig {
 
 	private static final String API_V1 = "/api/v1";
@@ -213,7 +224,7 @@ public class MultipleEntryPointsSecurityConfig {
 	 */
 	@Configuration
 	@Order(3)
-	public static class AdminConfigurationAdapter extends WebSecurityConfigurerAdapter {
+	public static class AdminConfigurationAdapter extends KeycloakWebSecurityConfigurerAdapter {
 
 		@Autowired
 		private WebUserServices userDetailsService;
@@ -221,18 +232,27 @@ public class MultipleEntryPointsSecurityConfig {
 		@Autowired
 		private UserAuthenticationSuccessHandler userAuthenticationSuccessHandler;
 
-		public AdminConfigurationAdapter() {
-			super();
+		/**
+		 * Registers the KeycloakAuthenticationProvider with the authentication manager.
+		 */
+		@Autowired
+		public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+			KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+			keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+			auth.authenticationProvider(keycloakAuthenticationProvider);
 		}
 
+		/**
+		 * Defines the session authentication strategy.
+		 */
+		@Bean
 		@Override
-		public void configure(AuthenticationManagerBuilder auth) throws Exception {
-			auth.userDetailsService(userDetailsService);
+		protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+			return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
 		}
-		
-		@Override
-		public void configure(WebSecurity web) {
-		}
+
+		@Bean
+		public KeycloakConfigResolver KeycloakConfigResolver() {return new KeycloakSpringBootConfigResolver();}
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
